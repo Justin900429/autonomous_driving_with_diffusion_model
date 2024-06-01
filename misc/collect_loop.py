2bac0306fe0b24466a93eb9633fd695031a375a5
Justin <justin900429@gmail.com>
2024-06-01 16:37:30 +0800

    ✨ Add loop feature for collecting data

---
+++ misc/collect_loop.py
@@ -0,0 +1,43 @@
+import argparse
+import glob
+import os
+import subprocess
+
+
+def count_current_saved(output_dir):
+    if not os.path.exists(output_dir):
+        return 0
+    else:
+        save_front = len(list(glob.glob(os.path.join(output_dir, "front/*.png"))))
+        save_bev = len(list(glob.glob(os.path.join(output_dir, "bev/*.png"))))
+        save_waypoints = len(list(glob.glob(os.path.join(output_dir, "waypoints/*.txt"))))
+        return min(save_front, save_bev, save_waypoints)
+
+
+def collect_loop(num_to_collect, output_dir):
+    cur_num = count_current_saved(output_dir)
+
+    while cur_num < num_to_collect:
+        # Do something
+        process = subprocess.Popen(
+            [
+                "python",
+                "misc/data_collect.py",
+                "--save-path",
+                output_dir,
+                "--save-num",
+                str(num_to_collect - cur_num),
+                "--off-screen",
+            ]
+        )
+        while process.poll() is None:
+            pass
+        cur_num = count_current_saved(output_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save-num", type=int, required=True)
+    parser.add_argument("--save-path", type=str, required=True)
+    args = parser.parse_args()
+    collect_loop(args.save_num, args.save_path)
