6503fa8ed5c2ffe7de35ade2cd1882457a2bd392
Justin <justin900429@gmail.com>
2024-06-04 21:25:23 +0800

    🚧 Work on guidance

--- control/guidance.py
+++ control/guidance.py
@@ -1,9 +1,22 @@
-import torch
+import importlib
+import os
+
+import torch.nn as nn
+
+DIR_NAME = os.path.dirname(__file__)
 
 
-class TargetGuidance(torch.nn.Module):
-    def __init__(self):
-        super(TargetGuidance, self).__init__()
-        
+class GuidanceLoss(nn.Module):
+    def __init__(self, loss_cls_list):
+        super(GuidanceLoss, self).__init__()
+
+        module = importlib.import_module(f"{DIR_NAME}.guidance_loss")
+        self.loss_list = nn.ModuleList([])
+        for loss_cls, loss_config in loss_cls_list:
+            self.loss_list.append(getattr(module, loss_cls)(**loss_config))
+
     def forward(self, x, target):
-        return torch.nn.functional.mse_loss(x[-1], target).mean()
+        total_loss = 0
+        for loss in self.loss_list:
+            total_loss += loss(x, target)
+        return total_loss
---
+++ control/guidance_loss.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TargetGuidance(nn.Module):
+    def __init__(self):
+        super(TargetGuidance, self).__init__()
+
+    def forward(self, x, target):
+        return F.mse_loss(x[-1], target).mean()
