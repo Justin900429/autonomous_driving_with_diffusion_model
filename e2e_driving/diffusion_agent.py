1c7ef6f4cf9c4c3562d87fef5802bf44ddd5c249
Justin <justin900429@gmail.com>
2024-06-06 21:55:03 +0800

    ✨ Add agent file for leaderboard evaluation

--- e2e_driving/diffusion_agent.py
+++ e2e_driving/diffusion_agent.py
@@ -10,13 +10,17 @@ import carla
 import cv2
 import numpy as np
 import torch
+from diffusers.schedulers import (DDIMScheduler, DDPMScheduler,
+                                  DPMSolverMultistepScheduler)
 from leaderboard.autoagents import autonomous_agent
 from PIL import Image
 from torchvision import transforms as T
 
-from e2e_driving.config import GlobalConfig
-from e2e_driving.model import Planner
+from config import create_cfg
+from control import GuidanceLoss
 from e2e_driving.planner import RoutePlanner
+from misc.load_param import copy_parameters
+from modeling import build_model
 
 SAVE_PATH = os.environ.get("SAVE_PATH", None)
 COLOR_LIST = [
@@ -25,6 +29,11 @@ COLOR_LIST = [
     (256, 126, 126),
     (255, 168, 168),
 ]
+SCHEDULER_FUNC = {
+    "ddpm": DDPMScheduler,
+    "ddim": DDIMScheduler,
+    "dpm": DPMSolverMultistepScheduler,
+}
 
 
 def way_point_to_pixel(waypoint):
@@ -33,10 +42,10 @@ def way_point_to_pixel(waypoint):
 
 
 def get_entry_point():
-    return "VAAgent"
+    return "DiffusionAgent"
 
 
-class VAAgent(autonomous_agent.AutonomousAgent):
+class DiffusionAgent(autonomous_agent.AutonomousAgent):
     def setup(self, path_to_conf_file):
         self.track = autonomous_agent.Track.SENSORS
         self.alpha = 0.3
@@ -51,25 +60,39 @@ class VAAgent(autonomous_agent.AutonomousAgent):
         self.wall_start = time.time()
         self.initialized = False
 
-        self.config = GlobalConfig()
-        self.net = Planner(self.config)
+        cfg = create_cfg()
+        if self.config_path is not None:
+            cfg.merge_from_file(self.config_path)
+        self.use_guidance = cfg.GUIDANCE.LOSS_LIST is not None
+        if self.use_guidance:
+            self.guidance_loss = GuidanceLoss(cfg)
+        self.config = cfg
+        self.model = build_model(self.config).to(self.device)
+        if cfg.EVAL.CHECKPOINT:
+            weight = torch.load(cfg.EVAL.CHECKPOINT, map_location=self.device)
+            self.model.load_state_dict(weight["state_dict"])
+            copy_parameters(weight["ema_state_dict"]["shadow_params"], self.model.parameters())
+            del weight
+            torch.cuda.empty_cache()
+            print("weights are loaded")
+        self.model.eval()
+        
+        scheduler_kwargs = dict(
+            num_train_timesteps=cfg.TRAIN.SAMPLE_STEPS,
+            prediction_type=cfg.TRAIN.NOISE_SCHEDULER.PRED_TYPE,
+            beta_schedule=cfg.TRAIN.NOISE_SCHEDULER.TYPE,
+            # For linear only
+            beta_start=cfg.TRAIN.NOISE_SCHEDULER.BETA_START,
+            beta_end=cfg.TRAIN.NOISE_SCHEDULER.BETA_END,
+            thresholding=True,
+        )
 
-        ckpt = torch.load(path_to_conf_file)
-        ckpt = ckpt["state_dict"]
-        new_state_dict = OrderedDict()
-        for key, value in ckpt.items():
-            new_key = key.replace("model.", "")
-            new_state_dict[new_key] = value
-        self.net.load_state_dict(new_state_dict)
-        self.net.cuda()
-        self.net.eval()
-
-        self.takeover = False
-        self.stop_time = 0
-        self.takeover_time = 0
+        if cfg.EVAL.SCHEDULER == "dpm":
+            scheduler_kwargs["lambda_min_clipped"] = -5.1
+        self.noise_scheduler = SCHEDULER_FUNC[cfg.EVAL.SCHEDULER](**scheduler_kwargs)
 
         self.save_path = None
-        self._im_transform = T.Compose(
+        self.image_transform = T.Compose(
             [
                 T.ToTensor(),
                 T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -81,7 +104,7 @@ class VAAgent(autonomous_agent.AutonomousAgent):
             string = pathlib.Path(os.environ["ROUTES"]).stem + "_"
             string += "_".join(
                 map(
-                    lambda x: "%02d" % x,
+                    lambda x: f"{x:02d}",
                     (now.month, now.day, now.hour, now.minute, now.second),
                 )
             )
@@ -98,13 +121,11 @@ class VAAgent(autonomous_agent.AutonomousAgent):
     def _init(self):
         self._route_planner = RoutePlanner(4.0, 50.0)
         self._route_planner.set_route(self._global_plan, True)
-
         self.initialized = True
 
     def _get_position(self, tick_data):
         gps = tick_data["gps"]
         gps = (gps - self._route_planner.mean) * self._route_planner.scale
-
         return gps
 
     def sensors(self):
@@ -159,6 +180,39 @@ class VAAgent(autonomous_agent.AutonomousAgent):
             },
             {"type": "sensor.speedometer", "reading_frequency": 20, "id": "speed"},
         ]
+        
+    def generate_traj(self, image, target=None):
+        self.model.eval()
+        traj_shape = (
+            1,
+            self.config.MODEL.HORIZON,
+            self.config.MODEL.TRANSITION_DIM,
+        )
+        trajs = torch.randn(traj_shape, device=self.device)
+        image = image.to(self.device)
+
+        trajs[:, 0, :2] = 0.0
+
+        self.noise_scheduler.set_timesteps(self.config.EVAL.SAMPLE_STEPS, device=self.device)
+        prev_t = None
+        for t in self.noise_scheduler.timesteps:
+            with torch.no_grad():
+                model_output = self.model(
+                    trajs,
+                    image,
+                    t.reshape(-1),
+                )
+            if t > 0 and prev_t is not None and self.use_guidance:
+                posterior_variance = self.noise_scheduler._get_variance(t, prev_t)
+                model_std = torch.exp(0.5 * posterior_variance)
+                model_output = self.guidance_loss(model_output, target, model_std)
+            trajs = self.noise_scheduler.step(model_output, t, trajs).prev_sample
+            trajs[:, 0, :2] = 0.0
+            prev_t = t
+
+        trajs = trajs.to(torch.float32).clamp(-1, 1)
+        trajs[..., :2] *= self.model.magic_num
+        return trajs
 
     def tick(self, input_data):
         self.step += 1
@@ -168,9 +222,7 @@ class VAAgent(autonomous_agent.AutonomousAgent):
         speed = input_data["speed"][1]["speed"]
         compass = input_data["imu"][1][-1]
 
-        if math.isnan(
-            compass
-        ):  # It can happen that the compass sends nan for a few frames
+        if math.isnan(compass):
             compass = 0.0
 
         result = {
@@ -190,10 +242,22 @@ class VAAgent(autonomous_agent.AutonomousAgent):
         R = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
         local_command_point = np.array([next_wp[0] - pos[0], next_wp[1] - pos[1]])
-        local_command_point = R.T.dot(local_command_point)
-        result["target_point"] = tuple(local_command_point)
+        local_command_point = R.T.dot(local_command_point).reshape(-1)
+        
+        result["target_point"] = np.array([local_command_point[1], -local_command_point[0]]) / self.model.magic_num
 
         return result
+    
+    def post_process_control(self, throttle_res, steer_res, brake_res):
+        if brake_res < 0.05:
+            brake_res = 0.0
+        if throttle_res > brake_res:
+            brake_res = 0.0
+
+        if brake_res > 0.5:
+            throttle_res = float(0)
+
+        return throttle_res, steer_res, brake_res
 
     @torch.no_grad()
     def run_step(self, input_data, timestamp):
@@ -201,130 +265,35 @@ class VAAgent(autonomous_agent.AutonomousAgent):
             self._init()
         tick_data = self.tick(input_data)
         if self.step < self.config.seq_len:
-            rgb = self._im_transform(tick_data["rgb"]).unsqueeze(0)
-
             control = carla.VehicleControl()
             control.steer = 0.0
             control.throttle = 0.0
             control.brake = 0.0
 
             return control
-
-        command = tick_data["next_command"]
-        if command < 0:
-            command = 4
-        command -= 1
-        assert command in [0, 1, 2, 3, 4, 5]
-        cmd_one_hot = [0] * 6
-        cmd_one_hot[command] = 1
-        cmd_one_hot = (
-            torch.tensor(cmd_one_hot).view(1, 6).to("cuda", dtype=torch.float32)
+        
+        target_point = torch.FloatTensor(tick_data["target_point"]).to(self.device)
+        image = self.image_transform(tick_data["rgb"]).unsqueeze(0).to(self.device)
+        traj = self.generate_traj(
+            image, target_point if self.use_guidance else None
         )
-        speed = (
-            torch.FloatTensor([float(tick_data["speed"])])
-            .view(1, 1)
-            .to("cuda", dtype=torch.float32)
+        
+        throttle, steer, brake  = self.post_process_control(*traj[0, 0, -3:].cpu().numpy())
+        control = carla.VehicleControl(
+            throttle=throttle, steer=steer, brake=brake
         )
-        speed = speed / 12
-        rgb = (
-            self._im_transform(tick_data["rgb"])
-            .unsqueeze(0)
-            .to("cuda", dtype=torch.float32)
-        )
-
-        tick_data["target_point"] = [
-            torch.FloatTensor([tick_data["target_point"][0]]),
-            torch.FloatTensor([tick_data["target_point"][1]]),
-        ]
-        target_point = torch.stack(tick_data["target_point"], dim=1).to(
-            "cuda", dtype=torch.float32
-        )
-        state = torch.cat([speed, target_point, cmd_one_hot], 1)
-
-        pred = self.net(rgb, state, target_point)
-
-        gt_velocity = torch.FloatTensor([tick_data["speed"]]).to(
-            "cuda", dtype=torch.float32
-        )
-        steer_ctrl, throttle_ctrl, brake_ctrl, metadata = self.net.process_action(
-            pred, tick_data["next_command"], gt_velocity, target_point
-        )
-
-        steer_traj, throttle_traj, brake_traj, metadata_traj = self.net.control_pid(
-            pred["pred_wp"], gt_velocity, target_point
-        )
-
-        if brake_traj < 0.05:
-            brake_traj = 0.0
-        if throttle_traj > brake_traj:
-            brake_traj = 0.0
-
-        self.pid_metadata = metadata_traj
-        self.pid_metadata["agent"] = "traj"
-
-        control = carla.VehicleControl()
-
-        if self.status == 0:
-            self.alpha = 0.3
-            self.pid_metadata["agent"] = "traj"
-            control.steer = np.clip(
-                self.alpha * steer_ctrl + (1 - self.alpha) * steer_traj, -1, 1
-            )
-            control.throttle = np.clip(
-                self.alpha * throttle_ctrl + (1 - self.alpha) * throttle_traj, 0, 0.75
-            )
-            control.brake = np.clip(
-                self.alpha * brake_ctrl + (1 - self.alpha) * brake_traj, 0, 1
-            )
-        else:
-            self.alpha = 0.3
-            self.pid_metadata["agent"] = "ctrl"
-            control.steer = np.clip(
-                self.alpha * steer_traj + (1 - self.alpha) * steer_ctrl, -1, 1
-            )
-            control.throttle = np.clip(
-                self.alpha * throttle_traj + (1 - self.alpha) * throttle_ctrl, 0, 0.75
-            )
-            control.brake = np.clip(
-                self.alpha * brake_traj + (1 - self.alpha) * brake_ctrl, 0, 1
-            )
-
-        self.pid_metadata["steer_ctrl"] = float(steer_ctrl)
-        self.pid_metadata["steer_traj"] = float(steer_traj)
-        self.pid_metadata["throttle_ctrl"] = float(throttle_ctrl)
-        self.pid_metadata["throttle_traj"] = float(throttle_traj)
-        self.pid_metadata["brake_ctrl"] = float(brake_ctrl)
-        self.pid_metadata["brake_traj"] = float(brake_traj)
-
-        if control.brake > 0.5:
-            control.throttle = float(0)
-        self.last_steers.append(abs(float(control.steer)))
-        num = 0
-        for s in self.last_steers:
-            if s > 0.10:
-                num += 1
-        if num > 10:
-            self.status = 1
-            self.steer_step += 1
-        else:
-            self.status = 0
-
-        self.pid_metadata["status"] = self.status
 
         if SAVE_PATH is not None and self.step % 10 == 0:
-            self.save(tick_data)
+            self.save(tick_data, traj[0, :, :2].cpu().numpy())
         return control
 
-    def save(self, tick_data):
+    def save(self, tick_data, traj_data):
         frame = self.step // 10
-        for color, key in zip(COLOR_LIST, ["wp_1", "wp_2", "wp_3", "wp_4"]):
-            waypoint = self.pid_metadata[key]
-            assert len(waypoint) == 2
-            pixel_x = way_point_to_pixel(-waypoint[0])
-            pixel_y = way_point_to_pixel(waypoint[1])
-            tick_data["bev"] = cv2.circle(
-                tick_data["bev"], (int(pixel_x), int(pixel_y)), 3, color, -1
-            )
+        for x, y in traj_data:
+            x, y = x / self.model.magic_num, y / self.model.magic_num
+            pixel_x = way_point_to_pixel(x)
+            pixel_y = way_point_to_pixel(y)
+            tick_data["bev"] = cv2.circle(tick_data["bev"], (pixel_x, pixel_y), 3, (0, 0, 255), -1)
         Image.fromarray(tick_data["rgb"]).save(
             self.save_path / "rgb" / ("%04d.png" % frame)
         )
@@ -332,10 +301,6 @@ class VAAgent(autonomous_agent.AutonomousAgent):
         Image.fromarray(tick_data["bev"]).save(
             self.save_path / "bev" / ("%04d.png" % frame)
         )
-
-        outfile = open(self.save_path / "meta" / ("%04d.json" % frame), "w")
-        json.dump(self.pid_metadata, outfile, indent=4)
-        outfile.close()
 
     def destroy(self):
         del self.net
