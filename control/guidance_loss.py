8728eacc86da183658e184ea8abf58636fc5254a
Justin <justin900429@gmail.com>
2024-06-07 16:15:28 +0800

    ✨ Add flexible approach to control the index

--- control/guidance_loss.py
+++ control/guidance_loss.py
@@ -3,10 +3,11 @@ import torch.nn.functional as F
 
 
 class TargetGuidance(nn.Module):
-    def __init__(self):
+    def __init__(self, target_index: int = 4):
         super(TargetGuidance, self).__init__()
+        self.target_index = target_index
 
     def forward(self, x, target):
         while target.dim() < x.dim() - 1:
             target = target.unsqueeze(0)
-        return F.mse_loss(x[:, -1, :2], target).mean()
+        return F.mse_loss(x[:, self.target_index, :2], target).mean()
