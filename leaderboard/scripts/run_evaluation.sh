6f60e85fe0c8072e16f3a483905e530932c15b8b
Justin <justin900429@gmail.com>
2024-06-11 00:44:16 +0800

    🚸 Add argument

--- leaderboard/scripts/run_evaluation.sh
+++ leaderboard/scripts/run_evaluation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export CARLA_ROOT=/home/justin/Documents/Carla_0.9.10
+if [ $# -ne 6 ]; then
+    echo "Usage: $0 <carla_server_path> <scenario_file> <route_file> <agent_config_file> <save_folder> <save_file>"
+    exit 1
+fi
+
+export CARLA_ROOT=${1}
 export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
@@ -17,12 +22,12 @@ export REPETITIONS=1 # multiple evaluation runs
 export RESUME=True
 
 # evaluation
-export SCENARIOS=leaderboard/data/scenarios/eval_scenarios.json
-export ROUTES=leaderboard/data/evaluation_routes/longest6_tiny.xml
-export TEAM_AGENT=e2e_driving/diffusion_agent.py 
-export TEAM_CONFIG=configs/guidance/free_guidance.yaml
-export CHECKPOINT_ENDPOINT=free_guidance_longest_5/free_guidance_longest_5.json 
-export SAVE_PATH=free_guidance_longest_5/ 
+export SCENARIOS=$2
+export ROUTES=$3
+export AGENT=e2e_driving/diffusion_agent.py 
+export AGENT_CONFIG=$4
+export SAVE_PATH=$5
+export CHECKPOINT_ENDPOINT=$6
 
 python ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
 --scenarios=${SCENARIOS}  \
@@ -30,8 +35,8 @@ python ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
 --repetitions=${REPETITIONS} \
 --track=${CHALLENGE_TRACK_CODENAME} \
 --checkpoint=${CHECKPOINT_ENDPOINT} \
---agent=${TEAM_AGENT} \
---agent-config=${TEAM_CONFIG} \
+--agent=${AGENT} \
+--agent-config=${AGENT_CONFIG} \
 --debug=${DEBUG_CHALLENGE} \
 --record=${RECORD_PATH} \
 --resume=${RESUME} \
