1d879a5e37e23fc101340e63cd7d80bbe33ff354
Justin <justin900429@gmail.com>
2024-06-11 21:57:13 +0800

    💥 Use dummy point to prevent weird steer

--- control/guidance_loss.py
+++ control/guidance_loss.py
@@ -12,9 +12,11 @@ class TargetGuidance(nn.Module):
             target = target.unsqueeze(0)
 
         loss_weight = F.softmin(torch.norm(target, dim=-1), dim=-1)
-        dist_matrix = (
-            torch.sum((x[..., :2].unsqueeze(1) - target.unsqueeze(2)) ** 2, dim=-1).amin(dim=-1)
-            * loss_weight
-        )  # (B, T)
-        loss = dist_matrix.sum()
-        return loss
+        dist_matrix = torch.sum((x[..., :2].unsqueeze(1) - target.unsqueeze(2)) ** 2, dim=-1)
+        target_to_agent = torch.norm(target - x[:, 0, :2], dim=-1)
+        final_to_agent = torch.norm(x[:, -1, :2] - x[:, 0, :2], dim=-1)
+        if final_to_agent < target_to_agent:
+            choose_target = 0  # Dummy point to prevent erratic update
+        else:
+            choose_target = dist_matrix.argmin(dim=-1)
+        return (dist_matrix[:, :, choose_target] * loss_weight).mean(dim=-1).sum()
